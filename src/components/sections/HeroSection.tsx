f396b7ea0c18fbb5845b6bbdb722403742067107
Nazarene <nazarenegena@users-MacBook-Pro.local>
2024-08-16 16:40:39 +0300

    updated landing page designs

--- src/components/sections/HeroSection.tsx
+++ src/components/sections/HeroSection.tsx
@@ -2,24 +2,24 @@ import React from "react";
 import { Button } from "../ui/button";
 import Link from "next/link";
 import Image from "next/image";
-import heroImage from "@/assets/jogging.svg";
-import yoga from "@/assets/girl-doing-yoga.svg";
-import mealplan from "@/assets/meal.svg";
-import scheduler from "@/assets/cycle-schedule.svg";
-import jogger from "@/assets/jogger.svg";
-import schedules from "@/assets/calendar.svg";
+import heroImage from "@/assets/lotus-yoga-pose.svg";
+import mealPlan from "@/assets/online-diet-plan.svg";
+import scheduler from "@/assets/calender.svg";
+import fitness from "@/assets/fitness.svg";
+import pilates from "@/assets/pilates.svg";
+import workout from "@/assets/workout.svg";
 
 type Props = {};
 
 const HeroSection = (props: Props) => {
   return (
-    <div className="mt-16">
-      <div className="grid grid-cols-2 gap-x-8 px-10">
-        <div className="flex flex-col justify-center items-center">
-          <p className="text-5xl mt-20 leading-relaxed tracking-wider font-medium text-neutral font-serif text-center">
-            Track your Active Lifestyle
+    <div className="mt-6">
+      <div className="grid grid-cols-3 px-10">
+        <div className="flex flex-col items-center justify-center ">
+          <p className="text-6xl leading-relaxed tracking-wider font-medium text-neutral font-serif text-center">
+            Track your <span className="text-primary/85">Active</span> Lifestyle
           </p>
-          <p className="text-muted-foreground mt-5">
+          <p className="text-muted-foreground mt-5 text-center">
             {" "}
             We offer fresh fitness schedules, workout regimes and mealplanners.{" "}
           </p>
@@ -27,28 +27,69 @@ const HeroSection = (props: Props) => {
             <Link href="/dashboard">Get Started</Link>
           </Button>
         </div>
+        <div className="w-[40rem] h-[45rem] relative ">
+          <Image
+            src={heroImage}
+            alt="fitness_app_logo"
+            fill
+            className=" object-contain"
+          />
+        </div>
 
-        <Image
-          src={heroImage}
-          alt="fitness_app_logo"
-          width={500}
-          height={500}
-          className="ml-24"
-        />
+        <div className="flex flex-col mt-20 items-end justify-center">
+          <div className="flex flex-col mr-12 mb-3 items-center justify-center">
+            <Image src={scheduler} alt="calender" width={50} />
+            <p className="mt-6 text-foreground font-semibold">
+              Schedule your Routine
+            </p>
+            <p className="text-muted-foreground text-xs mt-3">
+              Get to plan your workout regime
+            </p>
+            <hr className="w-64 h-px my-8 bg-muted border-0" />
+          </div>
+          <div className="flex flex-col mr-12 mb-3 items-center justify-center">
+            <Image src={mealPlan} alt="meal planning" width={50} />
+
+            <p className="flex flex-col mt-6 text-foreground">
+              Plan your Meals
+            </p>
+            <p className="text-muted-foreground text-xs mt-3">
+              Create your Weekly
+            </p>
+            <hr className="w-64 h-px my-8 bg-muted border-0" />
+          </div>
+          <div className="flex flex-col mr-12 mb-3 items-center justify-center">
+            <Image src={fitness} alt="yoga" width={50} />
+            <p className="mt-6 text-foreground">Customize your Workout </p>
+            <p className="text-muted-foreground text-xs mt-3">
+              Get to plan your workout regime
+            </p>
+            <hr className="w-64 h-px my-8 bg-muted border-0" />
+          </div>
+        </div>
       </div>
 
-      <div className="flex  gap-32 w- bg-primary/5 px-10 h-64 items-center justify-center">
-        <div className="flex flex-col mx-10 items-center justify-center">
-          <Image src={scheduler} alt="calender" width={100} />
-          <p className="mt-6 text-foreground">Schedule your Routine</p>
+      <div className="bg-primary/5 grid grid-cols-2 h-40">
+        <div className="border-r-2 border-primary/10 flex justify-center items-center">
+          <div className="mr-6">
+            <p className=" text-primary/60 font-medium">
+              Free Trial to begin with
+            </p>
+            <p className="text-2xl text-primary font-semibold tracking-wider">
+              Exclusive Classes
+            </p>
+          </div>
+
+          <Image src={pilates} alt="yoga" width={150} />
         </div>
-        <div className="mx-10 flex flex-col items-center justify-center">
-          <Image src={mealplan} alt="meal planning" width={100} />
-          <p className="mt-6 text-foreground">Plan your Meals</p>
-        </div>
-        <div className="mx-10 flex flex-col items-center justify-center">
-          <Image src={jogger} alt="yoga" width={100} />
-          <p className="mt-6 text-foreground">Customize your Workout </p>
+        <div className="flex justify-center items-center">
+          <div className="mr-6">
+            <p className=" text-primary/60 font-medium">Watch tutorials</p>
+            <p className="text-2xl text-primary font-semibold tracking-wider">
+              Personalized Training
+            </p>
+          </div>
+          <Image src={workout} alt="workout" width={150} />
         </div>
       </div>
     </div>
