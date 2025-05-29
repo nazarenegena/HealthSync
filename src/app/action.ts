ace6a49ae4b8ec3198596fa2bf5d54a580b734d0
Nazarene <nazarenegena@users-MacBook-Pro.local>
2025-05-29 10:57:19 +0300

    supabase config updated

--- src/app/action.ts
+++ src/app/action.ts
@@ -4,7 +4,6 @@ import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 
 import { createClient } from "@/utils/supabase/server";
-import { auth } from "@/config/firebase.config";
 
 export async function logout() {
   const supabase = await createClient();
